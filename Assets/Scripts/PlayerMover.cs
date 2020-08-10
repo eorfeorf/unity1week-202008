112a408dc76b2f41dfffcb480ab5ed45cb4442b0
EF <orerrurirar2@gmail.com>
2020-08-11 00:28:09 +0900

    プレイヤーの移動を実装

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private float speed = 1.0f;
+    
+    private Transform transform;
+
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        var velocity = Vector3.zero;
+        
+        velocity.x = Input.GetAxis("Horizontal");
+        velocity.z = Input.GetAxis("Vertical");
+
+        velocity = velocity.normalized * speed;
+        
+        Debug.Log(velocity);
+        transform.position += velocity * Time.deltaTime;
+    }
+}
